95073e3554c8075165a9feecf86817b97c0684b0
Zack Dowd <zdowd2796@gmail.com>
2024-02-09 11:55:03 -0700

    player movement working

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -36,7 +36,53 @@ public class Player : KinematicBody
 
     public override void _PhysicsProcess(float delta)
     {
-        base._PhysicsProcess(delta);
+        // reset x and z velocity
+        velocity.x = 0;
+        velocity.z = 0;
+
+        Vector2 input = Vector2.Zero;
+
+        // movement inputs
+        if (Input.IsActionPressed("move_forward"))
+        {
+            input.y -= 1;
+        }
+        if (Input.IsActionPressed("move_back"))
+        {
+            input.y += 1;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            input.x -= 1;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            input.x += 1;
+        }
+
+        input = input.Normalized();
+
+        // get forward and right directions
+        Vector3 forward = GlobalTransform.basis.z;
+        Vector3 right = GlobalTransform.basis.x;
+
+        Vector3 relativeDirection = forward * input.y + right * input.x;
+
+        //set velocity
+        velocity.x = relativeDirection.x * moveSpeed;
+        velocity.z = relativeDirection.z * moveSpeed;
+
+        // apply gravity
+        velocity.y -= gravity * delta;
+
+        // move player
+        velocity = MoveAndSlide(velocity, Vector3.Up);
+
+        // jumping
+        if (Input.IsActionPressed("jump") && IsOnFloor())
+        {
+            velocity.y = jumpForce;
+        }
     }
 
 }
